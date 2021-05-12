239da43708586cdabd76902f3e3317251dcbaf4f
cenxiaoxin <cenxiaoxin@forchange.tech>
2021-05-12 21:51:35 +0800

    feat: init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// import App from './App'
+// import reportWebVitals from './reportWebVitals'
+import { registerMicroApps, start } from 'qiankun'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const render = (appContent, loading) => {
+//   const container = document.getElementById('main')
+//   ReactDOM.render(
+//     <div dangerouslySetInnerHTML={{ __html: appContent }} />,
+//     container,
+//   )
+// }
+
+registerMicroApps([
+  {
+    name: 'qkm', // app name registered
+    entry: '//localhost:3001',
+    container: '#main',
+    activeRule: '/qkm',
+    // render,
+  },
+  // {
+  //   name: 'vue app',
+  //   entry: { scripts: ['//localhost:7100/main.js'] },
+  //   container: '#yourContainer2',
+  //   activeRule: '/yourActiveRule2',
+  // },
+])
+
+start()
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals()
